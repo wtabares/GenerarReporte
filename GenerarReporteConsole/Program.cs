0dc0ded4f4824ebb29ac3af179fecd0a097e1b2e
Wilfredo Tabares <wilfredo.tabares@gmail.com>
2021-06-08 12:21:53 -0500

    Get lines from IDS file

--- GenerarReporteConsole/Program.cs
+++ GenerarReporteConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 
@@ -8,16 +9,20 @@ namespace GenerarReporteConsole
     class Program
     {
 
-		public const string folder840Path = @"D:\EDI\EDI-840";
-		public const string folder843Path = @"D:\EDI\EDI-843";
-		public const string folder850Path = @"D:\EDI\EDI-850";
-
-		public const string tag840 = @"BQT";
-		public const string tag843 = @"BQR";
-		public const string tag850 = @"BEG";
+		public static string tag840 = @"BQT";
+		public static string tag843 = @"BQR";
+		public static string tag850 = @"BEG";
+		
+		public static string lin840 = @"PO1";
 
 		static int Main(string[] args)
         {
+
+			string rootFolder    = @ConfigurationManager.AppSettings["rootFolder"];
+			string folder840Path = @rootFolder + ConfigurationManager.AppSettings["folder840Path"];
+			string folder843Path = @rootFolder + ConfigurationManager.AppSettings["folder843Path"];
+			string folder850Path = @rootFolder + ConfigurationManager.AppSettings["folder850Path"];
+
 			try
             {
 				string dateInput;
@@ -32,11 +37,9 @@ namespace GenerarReporteConsole
 				}
 
 				ProcesarDirectorio(folder840Path, DateTime.Parse(dateInput), tag840);
-				DisplayBreak('>');
-				DisplayBreak('>');
+				DisplayBreak('>', 2);
 				ProcesarDirectorio(folder843Path, DateTime.Parse(dateInput), tag843);
-				DisplayBreak('>');
-				DisplayBreak('>');
+				DisplayBreak('>', 2);
 				ProcesarDirectorio(folder850Path, DateTime.Parse(dateInput), tag850);
 				return 0;
 			}
@@ -74,10 +77,12 @@ namespace GenerarReporteConsole
 			String fileName = Path.GetFileName(pathFile);
 			DateTime lastMod = File.GetLastWriteTime(pathFile);
 			List<string> current = new List<string>();
+			List<int> lineas = new List<int>();
 
 			try
 			{
 				int currentLine = 1;
+				int numLineas = 0;
 				// Open the file text using a stream reader.
 				foreach (var line in File.ReadAllLines(pathFile))
 				{
@@ -95,6 +100,8 @@ namespace GenerarReporteConsole
 								!string.IsNullOrEmpty(line) ? line.Split(' ', StringSplitOptions.RemoveEmptyEntries) : Enumerable.Empty<string>();
 							string token = tokens.Last();
 							current.Add(token.Substring(0, token.Length-1));
+							lineas.Add(numLineas);
+							numLineas = 0;
 						}
 						else if (tag843.Equals(tag))
 						{
@@ -103,17 +110,28 @@ namespace GenerarReporteConsole
 								!string.IsNullOrEmpty(line) ? line.Split(' ', StringSplitOptions.RemoveEmptyEntries) : Enumerable.Empty<string>();
 							string token = tokens.Last();
 							current.Add(token.Substring(0, token.Length - 1));
+							lineas.Add(numLineas);
+							numLineas = 0;
 						}
 						else if (tag850.Equals(tag))
 						{
 							// agregar logica para prop 850 - BEG)
 							string token = !string.IsNullOrEmpty(line) ? line.Substring(65, 30) : "";
 							current.Add(token.Trim());
+							lineas.Add(numLineas);
+							numLineas = 0;
 						}
 					}
+
+					// identificar líneas de las prop edi840
+					if (line.Contains(lin840))
+                    {
+						numLineas++;
+                    }
 					currentLine++;
 				}
-				DisplayData(fileName, lastMod.ToString(), numeroControl, current);
+				lineas.Add(numLineas);
+				DisplayData(fileName, lastMod.ToString(), numeroControl, current, lineas);
 			}
 			catch (IOException e)
 			{
@@ -122,13 +140,14 @@ namespace GenerarReporteConsole
 		}
 
 
-		public static void DisplayData(string fileName, string lastMod, string noControl, List<string> props)
+		public static void DisplayData(string fileName, string lastMod, string noControl, List<string> props, List<int> lineas)
         {
 			bool isEmpty = props.Count == 0;
+			bool isLineasEmpty = lineas.Count == 0;
 
 			if (isEmpty)
             {
-				Console.WriteLine("{0},{1},{2},{3}", fileName, lastMod, noControl, "NO DATA");
+				Console.WriteLine("{0},{1},{2},{3},{4}", fileName, lastMod, noControl, "NO DATA", "NO LINES");
 			}
 			else
             {
@@ -136,26 +155,29 @@ namespace GenerarReporteConsole
 				int lin = 1;
 				foreach (var item in props)
 				{
+					int numLineas = !isLineasEmpty ? lineas.ElementAt(lin) : 0;
 					if (lin == 1)
 					{
-						Console.WriteLine("{0},{1},{2},{3}", fileName, lastMod, noControl, item.Trim());
-						lin++;
+						Console.WriteLine("{0},{1},{2},{3},{4}", fileName, lastMod, noControl, item.Trim(), numLineas);
 					}
 					else
 					{
-						Console.WriteLine(",,,{0}", item.Trim());
+						Console.WriteLine(",,,{0},{1}", item.Trim(), numLineas);
 					}
+					lin++;
 				}
 			}
-			DisplayBreak('*');
+			DisplayBreak('*', 1);
 		}
 
-		public static void DisplayBreak(Char caracter)
+		public static void DisplayBreak(Char caracter, int times)
         {
-			Console.WriteLine();
-			Console.WriteLine(new string(caracter, 100));
-			Console.WriteLine();
+			for (int i = 1; i <= times; i++)
+			{ 
+				Console.WriteLine();
+				Console.WriteLine(new string(caracter, 100));
+				Console.WriteLine();
+			}
 		}
-
 	}
 }
